e2f424589978e7f29f3b87b37818805e1de28223
CHK <chenghuangkai@163.com>
2019-05-17 10:13:51 +0800

    完成按钮弹出5个控件

--- app/src/main/java/com/potevio/customviewtest/SplashActivity.java
+++ app/src/main/java/com/potevio/customviewtest/SplashActivity.java
@@ -1,10 +1,12 @@
 package com.potevio.customviewtest;
 
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.BounceInterpolator;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -39,7 +41,8 @@ public class SplashActivity extends AppCompatActivity {
     float fixXPosition;
     float fixYPosition;
 
-    float length = 200f;
+    int length = 400;
+    int duration = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,11 +57,23 @@ public class SplashActivity extends AppCompatActivity {
     void onClick(View view) {
         switch (view.getId()) {
             case R.id.fixButton:
-                Toast.makeText(this,"you click the fixButton",Toast.LENGTH_SHORT).show();
-                if (!isOpen)
-                    openAnimator();
-                else
-                    closeAnimator();
+                if (!isOpen) {
+                    doOpenFixAnimator(fixButton);
+                    doOpenAnimator(showButton1,0,4,length);
+                    doOpenAnimator(showButton2,1,4,length);
+                    doOpenAnimator(showButton3,2,4,length);
+                    doOpenAnimator(showButton4,3,4,length);
+                    doOpenAnimator(showButton5,4,4,length);
+                    isOpen = true;
+                } else {
+                    doCloseFixAnimator(fixButton);
+                    doCloseAnimator(showButton1,0,4,length);
+                    doCloseAnimator(showButton2,1,4,length);
+                    doCloseAnimator(showButton3,2,4,length);
+                    doCloseAnimator(showButton4,3,4,length);
+                    doCloseAnimator(showButton5,4,4,length);
+                    isOpen = false;
+                }
                 break;
             case R.id.showButton1:
                 Toast.makeText(this, "you click the button 1", Toast.LENGTH_SHORT).show();
@@ -85,24 +100,120 @@ public class SplashActivity extends AppCompatActivity {
     }
 
     void openAnimator() {
-        Log.i(TAG,showButton1.getX()+"  "+showButton1.getWidth()+"   "+showButton1.getLeft());
-        Log.i(TAG,showButton1.getY()+"  "+showButton1.getWidth()+"   "+showButton1.getTop());
         isOpen = true;
-//        ObjectAnimator button1ObjectAnimator = ObjectAnimator.ofFloat(showButton1,"translationY",showButton1.getY(),(showButton1.getY()-length));
-//        button1ObjectAnimator.setDuration(1000);
-//        button1ObjectAnimator.start();
+        ObjectAnimator button1YObjectAnimator = ObjectAnimator.ofFloat(showButton1,"translationY",0f,-length);
+        ObjectAnimator button1XObjectAnimator = ObjectAnimator.ofFloat(showButton1,"translationX",0f,0);
 
-        ObjectAnimator fixObjectAnimator = ObjectAnimator.ofFloat(fixButton,"translationX",fixButton.getLeft(),fixButton.getLeft()-length);
-        fixObjectAnimator.setDuration(1000);
-        fixObjectAnimator.start();
+        ObjectAnimator button2YObjectAnimator = ObjectAnimator.ofFloat(showButton2,"translationY",0f,-(float)(length * Math.cos(Math.PI/8)));
+        ObjectAnimator button2XObjectAnimator = ObjectAnimator.ofFloat(showButton2,"translationX",0f,-(float)(length * Math.sin(Math.PI/8)));
+
+
+        ObjectAnimator button3YObjectAnimator = ObjectAnimator.ofFloat(showButton3,"translationY",0f,-(float)(length * Math.cos(Math.PI/4)));
+        ObjectAnimator button3XObjectAnimator = ObjectAnimator.ofFloat(showButton3,"translationX",0f,-(float)(length * Math.sin(Math.PI/4)));
+
+
+        ObjectAnimator button4YObjectAnimator = ObjectAnimator.ofFloat(showButton4,"translationY",0f,-(float)(length * Math.cos(Math.PI * 3/8)));
+        ObjectAnimator button4XObjectAnimator = ObjectAnimator.ofFloat(showButton4,"translationX",0f,-(float)(length * Math.sin(Math.PI * 3/8)));
+
+
+        ObjectAnimator button5YObjectAnimator = ObjectAnimator.ofFloat(showButton5,"translationY",0f,0);
+        ObjectAnimator button5XObjectAnimator = ObjectAnimator.ofFloat(showButton5,"translationX",0f,-(float)(length * Math.sin(Math.PI/2)));
+
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(button1XObjectAnimator,button1YObjectAnimator,
+                button2XObjectAnimator,button2YObjectAnimator,
+                button3XObjectAnimator,button3YObjectAnimator,
+                button4XObjectAnimator,button4YObjectAnimator,
+                button5XObjectAnimator,button5YObjectAnimator);
+        animatorSet.setDuration(duration);
+        animatorSet.start();
     }
 
     void closeAnimator() {
         isOpen = false;
-        Log.i(TAG,showButton1.getX()+"  "+showButton1.getWidth()+"   "+showButton1.getLeft());
-        Log.i(TAG,showButton1.getY()+"  "+showButton1.getWidth()+"   "+showButton1.getTop());
-        ObjectAnimator button1ObjectAnimator = ObjectAnimator.ofFloat(showButton1,"translationY",showButton1.getY(),(showButton1.getY()+length));
-        button1ObjectAnimator.setDuration(1000);
-        button1ObjectAnimator.start();
+        ObjectAnimator button1YObjectAnimator = ObjectAnimator.ofFloat(showButton1,"translationY",-length,0f);
+        ObjectAnimator button1XObjectAnimator = ObjectAnimator.ofFloat(showButton1,"translationX",0f,0);
+
+        ObjectAnimator button2YObjectAnimator = ObjectAnimator.ofFloat(showButton2,"translationY",-(float)(length * Math.cos(Math.PI/8)),0f);
+        ObjectAnimator button2XObjectAnimator = ObjectAnimator.ofFloat(showButton2,"translationX",-(float)(length * Math.sin(Math.PI/8)),0f);
+
+
+        ObjectAnimator button3YObjectAnimator = ObjectAnimator.ofFloat(showButton3,"translationY",-(float)(length * Math.cos(Math.PI/4)),0f);
+        ObjectAnimator button3XObjectAnimator = ObjectAnimator.ofFloat(showButton3,"translationX",-(float)(length * Math.sin(Math.PI/4)),0f);
+
+
+        ObjectAnimator button4YObjectAnimator = ObjectAnimator.ofFloat(showButton4,"translationY",-(float)(length * Math.cos(Math.PI * 3/8)),0f);
+        ObjectAnimator button4XObjectAnimator = ObjectAnimator.ofFloat(showButton4,"translationX",-(float)(length * Math.sin(Math.PI * 3/8)),0f);
+
+
+        ObjectAnimator button5YObjectAnimator = ObjectAnimator.ofFloat(showButton5,"translationY",0f,0);
+        ObjectAnimator button5XObjectAnimator = ObjectAnimator.ofFloat(showButton5,"translationX",-(float)(length * Math.sin(Math.PI/2)),0f);
+
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(button1XObjectAnimator,button1YObjectAnimator,
+                button2XObjectAnimator,button2YObjectAnimator,
+                button3XObjectAnimator,button3YObjectAnimator,
+                button4XObjectAnimator,button4YObjectAnimator,
+                button5XObjectAnimator,button5YObjectAnimator);
+        animatorSet.setDuration(duration);
+        animatorSet.start();
     }
+
+    void doOpenFixAnimator(View view) {
+        ObjectAnimator angleObjectAnimator = ObjectAnimator.ofFloat(view,"rotation",0,45);
+        angleObjectAnimator.setDuration(duration);
+        angleObjectAnimator.start();
+    }
+
+    void doCloseFixAnimator(View view) {
+        ObjectAnimator angleObjectAnimator = ObjectAnimator.ofFloat(view,"rotation",45,0);
+        angleObjectAnimator.setDuration(duration);
+        angleObjectAnimator.start();
+    }
+
+
+    void doOpenAnimator(View view,float index,float total,int length) {
+        double angle = index / total * Math.PI / 2;
+        float detalX = (float) (length * Math.sin(angle));
+        float detalY = (float) (length * Math.cos(angle));
+
+        ObjectAnimator objectXAnimator = ObjectAnimator.ofFloat(view,"translationX",0,-detalX);
+        ObjectAnimator objectYAnimator = ObjectAnimator.ofFloat(view,"translationY",0,-detalY);
+        ObjectAnimator objectAlphaAnimator = ObjectAnimator.ofFloat(view,"alpha",0,1);
+        ObjectAnimator objectScaleXAnimator = ObjectAnimator.ofFloat(view,"scaleX",0.1f,1);
+        ObjectAnimator objectScaleYAnimator = ObjectAnimator.ofFloat(view,"scaleY",0.1f,1);
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(objectXAnimator,objectYAnimator,objectAlphaAnimator,objectScaleXAnimator,objectScaleYAnimator);
+        animatorSet.setDuration(duration);
+        animatorSet.start();
+    }
+
+    void doCloseAnimator(View view,float index,float total,int length) {
+        double angle = index / total * Math.PI / 2;
+        float detalX = (float) (length * Math.sin(angle));
+        float detalY = (float) (length * Math.cos(angle));
+
+        ObjectAnimator objectXAnimator = ObjectAnimator.ofFloat(view,"translationX",-detalX,0);
+        ObjectAnimator objectYAnimator = ObjectAnimator.ofFloat(view,"translationY",-detalY,0);
+        ObjectAnimator objectAlphaAnimator = ObjectAnimator.ofFloat(view,"alpha",1,0);
+        ObjectAnimator objectScaleXAnimator = ObjectAnimator.ofFloat(view,"scaleX",1,0.1f);
+        ObjectAnimator objectScaleYAnimator = ObjectAnimator.ofFloat(view,"scaleY",1,0.1f);
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(objectXAnimator,objectYAnimator,objectAlphaAnimator,objectScaleXAnimator,objectScaleYAnimator);
+        animatorSet.setDuration(duration);
+        animatorSet.start();
+    }
+
+
+
+
+
+
+
+
+
 }
